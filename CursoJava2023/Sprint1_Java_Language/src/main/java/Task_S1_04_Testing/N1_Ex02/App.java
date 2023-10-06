05b9d90fb439bb6489a55e07f578545f907c6cf1
Santisl91 <santiagosantoslapel@gmail.com>
2023-10-06 13:52:33 +0200

    actualizado

--- CursoJava2023/Sprint1_Java_Language/src/main/java/Task_S1_04_Testing/N1_Ex02/App.java
+++ CursoJava2023/Sprint1_Java_Language/src/main/java/Task_S1_04_Testing/N1_Ex02/App.java
@@ -1,11 +1,40 @@
 package Task_S1_04_Testing.N1_Ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
-        CalculoDni numDNI = new CalculoDni(345678567);
-        char letraDNI = CalculoDni.calcularLetrasDNI(numDNI.getNumeroDni());
+        List<CalculoDni> listaDNI = new ArrayList<>();
 
-        System.out.println("la letra del DNI " + numDNI + " es:" + letraDNI);
+        CalculoDni numDNI1 = new CalculoDni(345678567);
+        CalculoDni numDNI2 = new CalculoDni(123234345);
+        CalculoDni numDNI3 = new CalculoDni(345456567);
+        CalculoDni numDNI4 = new CalculoDni(321432543);
+        CalculoDni numDNI5 = new CalculoDni(654765876);
+        CalculoDni numDNI6 = new CalculoDni(987876765);
+        CalculoDni numDNI7 = new CalculoDni(456765345);
+        CalculoDni numDNI8 = new CalculoDni(543456543);
+        CalculoDni numDNI9 = new CalculoDni(334455334);
+        CalculoDni numDNI10 = new CalculoDni(222333444);
+
+        listaDNI.add(numDNI1);
+        listaDNI.add(numDNI2);
+        listaDNI.add(numDNI3);
+        listaDNI.add(numDNI4);
+        listaDNI.add(numDNI5);
+        listaDNI.add(numDNI6);
+        listaDNI.add(numDNI7);
+        listaDNI.add(numDNI8);
+        listaDNI.add(numDNI9);
+        listaDNI.add(numDNI10);
+
+
+        for (CalculoDni dni : listaDNI) {
+            char letraDNI = CalculoDni.calcularLetrasDNI(dni.getNumeroDni());
+            System.out.println("la letra del DNI " + dni.getNumeroDni() + " es:" + letraDNI);
+
+        }
     }
 }
--- CursoJava2023/Sprint1_Java_Language/src/test/java/Task_S1_04_Testing/N1_Ex02/CalculoDniTest.java
+++ CursoJava2023/Sprint1_Java_Language/src/test/java/Task_S1_04_Testing/N1_Ex02/CalculoDniTest.java
@@ -9,9 +9,10 @@ class CalculoDniTest {
     @Test
     public void testCalcularLetraDNI() {
 
-        int[] numeroDNI = {345678567};
+        int[] numeroDNI = {345678567, 123234345, 345456567, 321432543, 654765876, 987876765, 456765345, 543456543,
+                334455334, 222333444};
 
-        char[] letraEsperada = {'K'};
+        char[] letraEsperada = {'K', 'T', 'V', 'E', 'J', 'Q', 'L', 'P', 'Y', 'B'};
 
         for (int i = 0; i < numeroDNI.length; i++) {
             char letraCalculada = CalculoDni.calcularLetrasDNI(numeroDNI[i]);
